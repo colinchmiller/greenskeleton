ebe9597bf61068592f7d69689b212c44d66924b7
colinmiller <colinchmiller@gmail.com>
2016-02-25 21:40:44 -0600

    add basic route for plant library and test successful

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,19 @@
-var mainApp = angular.module('mainApp', []);
+var mainApp = angular.module('mainApp', ['ngRoute']);
+
+mainApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/library', {
+      templateUrl: 'assets/views/routes/library.html',
+      controller: 'LibraryController'
+    })
+}]);
 
 mainApp.controller('TestController', ['$scope', function($scope){
   console.log("TestController firing");
   $scope.test = "Test of the controller connection."
+}]);
+
+mainApp.controller('LibraryController', ['$scope', function($scope){
+  console.log("LibraryController firing");
+  $scope.test = "Plant library route functioning."
 }]);
